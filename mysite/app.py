183d20afce6b2a8e6e66bd01fe33ccd2f1b75dab
PhyoTP <phyo.thet.pai@sji.edu.sg>
2024-08-06 02:16:51 +0000

    database persists

--- mysite/app.py
+++ mysite/app.py
@@ -1,18 +1,49 @@
 from flask import Flask, request, jsonify
+import sqlite3
+import json
 
 app = Flask(__name__)
 
-items = []
+def get_db_connection():
+    conn = sqlite3.connect("multicards.db")
+    conn.row_factory = sqlite3.Row  # To return rows as dictionaries
+    return conn
+
+def create_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS sets(
+        setID INTEGER PRIMARY KEY,
+        name TEXT,
+        cards TEXT)''')
+    conn.commit()
+    conn.close()
+
+create_table()
 
 @app.route('/sets', methods=['GET'])
 def get_items():
-    return jsonify(items)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sets')
+    sets = cur.fetchall()
+    conn.close()
+
+    # Convert the fetched rows to a list of dictionaries
+    sets_list = [dict(row) for row in sets]
+    return jsonify(sets_list)
 
 @app.route('/sets', methods=['POST'])
 def add_item():
-    item = request.json
-    items.append(item)
-    return jsonify(item), 201
+    new_set = request.json
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO sets (name, cards) VALUES (?, ?)', (new_set["name"], json.dumps(new_set["cards"])))
+    conn.commit()
+    conn.close()
+
+    return jsonify(new_set), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
