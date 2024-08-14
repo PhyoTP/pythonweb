2059a89793c5fa6a2a2c663cefb740553c2239d5
PhyoTP <phyo.thet.pai@sji.edu.sg>
2024-08-14 05:26:12 +0000

    added userbase

--- mysite/multicards/pages.py
+++ mysite/multicards/pages.py
@@ -13,24 +13,25 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row  # To return rows as dictionaries
     return conn
 
-def create_table():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS setstable(
-        setID INTEGER PRIMARY KEY,
-        name TEXT,
-        cards TEXT,
-        creator VARCHAR(100)
-    )''')
-    conn.commit()
-    conn.close()
 
-create_table()
+conn = get_db_connection()
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS setstable(
+    setID INTEGER PRIMARY KEY,
+    name TEXT,
+    cards TEXT,
+    creator VARCHAR(100)
+)''')
+conn.commit()
+cur.execute('''CREATE TABLE IF NOT EXISTS userbase(
+    userID TEXT PRIMARY KEY,
+    name VARCHAR(100)
+)''')
+conn.commit()
 
-@bp.route('/multicards/api', methods=['GET'])
-def get_items():
-    conn = get_db_connection()
-    cur = conn.cursor()
+
+@bp.route('/api/multicards/sets', methods=['GET'])
+def get_sets():
     cur.execute('SELECT * FROM setstable')
     sets_table = cur.fetchall()
     conn.close()
@@ -42,14 +43,48 @@ def get_items():
 
     return jsonify(sets_list)
 
-@bp.route('/multicards/api', methods=['POST'])
-def add_item():
+@bp.route('/api/multicards/sets', methods=['POST'])
+def add_set():
     new_set = request.json
 
-    conn = get_db_connection()
-    cur = conn.cursor()
     cur.execute('INSERT INTO setstable (name, cards, creator) VALUES (?, ?, ?)', (new_set["name"], json.dumps(new_set["cards"]), new_set["creator"]))
     conn.commit()
     conn.close()
 
     return jsonify(new_set), 201
+
+@bp.route('/api/multicards/user/<uuid>', methods=['GET'])
+def get_user(uuid):
+    cur.execute('SELECT * FROM userbase WHERE userID = ?', (uuid,))
+    user = cur.fetchone()
+    conn.close()
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Convert the row to a dictionary
+    user_dict = dict(user)
+
+    return jsonify(user_dict)
+@bp.route('/api/multicards/user',methods=['POST'])
+def add_user():
+    new_user = request.json
+    if not new_user:
+        return jsonify({'error': 'Invalid input'}), 400
+
+    try:
+        uuid, username = list(new_user.items())[0]
+    except ValueError:
+        return jsonify({'error': 'Invalid format'}), 400
+
+
+    # Insert the UUID and username into the users table
+    try:
+        cur.execute('INSERT INTO userbase (userID, name) VALUES (?, ?)', (uuid, username))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        conn.close()
+        return jsonify({'error': 'User with this UUID already exists or username is not unique'}), 400
+
+    conn.close()
+
+    return jsonify({'message': 'User added successfully'}), 201
