08306a3f37c6ab79cd75e0967f37953b562b3b0a
PhyoTP <phyo.thet.pai@sji.edu.sg>
2025-07-16 13:34:27 +0000

    remove unneeded files from git and add timekeeper

--- mysite/timekeeper/pages.py
+++ mysite/timekeeper/pages.py
@@ -1,6 +1,11 @@
 from flask import Blueprint, request, jsonify
+from timekeeper.timekeeper import app as slack_app
+from slack_bolt.adapter.flask import SlackRequestHandler
 
 bp = Blueprint('timekeeper', __name__)
 
-# @bp.route('/timekeeper', methods=['POST'])
-# def timekeeper():
+handler = SlackRequestHandler(slack_app)
+
+@bp.route('/timekeeper', methods=['POST'])
+def timekeeper():
+    return handler.handle(request)
